a63a5e28ef4797736f75a78be90c60159465ece3
OlehVelychko <70134299+OlehVelychko@users.noreply.github.com>
2024-11-11 16:03:42 +0200

    Fix token generation logic

--- src/main/java/com/example/smartbusinessapp/SmartBusinessAppApplication.java
+++ src/main/java/com/example/smartbusinessapp/SmartBusinessAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartBusinessAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartBusinessAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBusinessAppApplication.class, args);
+    }
 
 }
