9e30be8a5459c082c0796599877ef10c1cba0b68
OlehVelychko <70134299+OlehVelychko@users.noreply.github.com>
2024-11-11 17:42:57 +0200

    “Changed @Controller to @RestController in HelloController to return response body directly and resolved MVC view resolution issue.”

--- src/main/java/com/example/smartbusinessapp/controllers/HelloController.java
+++ src/main/java/com/example/smartbusinessapp/controllers/HelloController.java
@@ -1,16 +1,16 @@
 package com.example.smartbusinessapp.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController  // Используйте @RestController вместо @Controller
 @RequestMapping
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
         System.out.println("Hello World");
-        return "Hello, World!";
+        return "Hello, World!";  // Это будет отправлено как тело ответа
     }
 }
